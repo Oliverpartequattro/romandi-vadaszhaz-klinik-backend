3a1f61676c34ccbd033a9d9722e2ae0cd152e9b8
Roncz <roncz.oliver@students.jedlik.eu>
2026-02-08 23:59:29 +0100

    Add basic folder structure

--- models/Record.js
+++ models/Record.js
@@ -5,7 +5,7 @@ const recordSchema = new mongoose.Schema({
     doctor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
     appointment_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Appointment' },
     service_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Service' },
-    description: { type: String, required: true }, // Vizit leírása
+    description: { type: String, required: true }, 
 }, { timestamps: true });
 
 module.exports = mongoose.model('Record', recordSchema);
