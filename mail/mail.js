dacbbc2ed2cb5df444bda7eb20a57a7762f8c790
Oliverpartequattro <oliverroncz2@gmail.com>
2026-02-13 09:53:54 +0100

    Add txt.txt.txt

--- mail/mail.js
+++ mail/mail.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 // Create a transporter using Ethereal test credentials.
 // For production, replace with your actual SMTP server details.
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
+  host: "smtp.gmail.com", // Ez a smtp.gmail.com közvetlen IPv4 címe
   port: 587,
   secure: false, // Use true for port 465, false for port 587
   auth: {
@@ -15,8 +15,8 @@ const transporter = nodemailer.createTransport({
 // Send an email using async/await
 (async () => {
   const info = await transporter.sendMail({
-    from: '"Maddison Foo Koch" <oliverroncz@gmail.com>',
-    to: "roncz.oliver@students.jedclik.eu",
+    from: '"Szemethy" <oliverroncz@gmail.com>',
+    to: "roncz.oliver@students.jedlik.eu",
     subject: "Hello ✔",
     text: "Hello world?", // Plain-text version of the message
     html: "<b>Hello world?</b>", // HTML version of the message
