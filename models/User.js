5f40487d9b341f8d736b986a66fda027dfe28c88
Roncz <roncz.oliver@students.jedlik.eu>
2026-05-19 09:15:48 +0200

    Update phone number regex

--- models/User.js
+++ models/User.js
@@ -66,7 +66,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Telefonszám megadása kötelező"],
       match: [
-        /^(?:\+36|06)(?:20|30|31|70)\d{7}$/,
+        /^(?:36|06)(?:20|30|31|70)\d{7}$/,
         "Érvénytelen telefonszám. Példa: +36301234567"
       ],
     },
